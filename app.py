dfc71accdb7ba4aae7251a0c7db98490fde2ab0b
poonyma <138734368+poonyma@users.noreply.github.com>
2025-07-08 18:42:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -13,24 +13,24 @@ from threading import Thread
 from http.server import BaseHTTPRequestHandler, HTTPServer
 
 # Environment variables
-UPLOAD_URL = os.environ.get('UPLOAD_URL', '')          # 节点或订阅上传地址,只填写这个地址将上传节点,同时填写PROJECT_URL将上传订阅，例如：https://merge.serv00.net
-PROJECT_URL = os.environ.get('PROJECT_URL', '')        # 项目url,需要自动保活或自动上传订阅需要填写,例如：https://www.google.com,
-AUTO_ACCESS = os.environ.get('AUTO_ACCESS', 'false').lower() == 'true'  # false关闭自动保活, true开启自动保活，默认关闭
-FILE_PATH = os.environ.get('FILE_PATH', './.cache')    # 运行路径,sub.txt保存路径
-SUB_PATH = os.environ.get('SUB_PATH', 'sub')           # 订阅token,默认sub，例如：https://www.google.com/sub
-UUID = os.environ.get('UUID', '20e6e496-cf19-45c8-b883-14f5e11cd9f1')  # UUID,如使用哪吒v1,在不同的平台部署需要修改,否则会覆盖
-NEZHA_SERVER = os.environ.get('NEZHA_SERVER', '')      # 哪吒面板域名或ip, v1格式: nezha.xxx.com:8008, v0格式: nezha.xxx.com
-NEZHA_PORT = os.environ.get('NEZHA_PORT', '')          # v1哪吒请留空, v0哪吒的agent通信端口,自动匹配tls
-NEZHA_KEY = os.environ.get('NEZHA_KEY', '')            # v1哪吒的NZ_CLIENT_SECRET或v0哪吒agent密钥
-ARGO_DOMAIN = os.environ.get('ARGO_DOMAIN', '')        # Argo固定隧道域名,留空即使用临时隧道
-ARGO_AUTH = os.environ.get('ARGO_AUTH', '')            # Argo固定隧道密钥,留空即使用临时隧道
-ARGO_PORT = int(os.environ.get('ARGO_PORT', '8001'))   # Argo端口,使用固定隧道token需在cloudflare后台设置端口和这里一致
-CFIP = os.environ.get('CFIP', 'www.visa.com.tw')       # 优选ip或优选域名
-CFPORT = int(os.environ.get('CFPORT', '443'))          # 优选ip或优选域名对应端口
-NAME = os.environ.get('NAME', 'Vls')                   # 节点名称
-CHAT_ID = os.environ.get('CHAT_ID', '')                # Telegram chat_id,推送节点到tg,两个变量同时填写才会推送
-BOT_TOKEN = os.environ.get('BOT_TOKEN', '')            # Telegram bot_token
-PORT = int(os.environ.get('SERVER_PORT') or os.environ.get('PORT') or 3000) # 订阅端口，如无法订阅，请手动修改为分配的端口
+UPLOAD_URL = os.environ.get('UPLOAD_URL', '')
+PROJECT_URL = os.environ.get('PROJECT_URL', ''
+AUTO_ACCESS = os.environ.get('AUTO_ACCESS', 'false').lower() == 'true'
+FILE_PATH = os.environ.get('FILE_PATH', './.cache')
+SUB_PATH = os.environ.get('SUB_PATH', 'sub')
+UUID = os.environ.get('UUID', '50693817-6164-4441-8156-3439a3301219')
+NEZHA_SERVER = os.environ.get('NEZHA_SERVER', '')
+NEZHA_PORT = os.environ.get('NEZHA_PORT', '')
+NEZHA_KEY = os.environ.get('NEZHA_KEY', '')
+ARGO_DOMAIN = os.environ.get('ARGO_DOMAIN', 'wangergo.leziren.eu.org')
+ARGO_AUTH = os.environ.get('ARGO_AUTH', 'eyJhIjoiZmQ3NmNiODBiZTBhOWMzMzY0MjE1Mzg1NTUwM2RkZDEiLCJ0IjoiZjYzYjExYzctMjk1OC00NTgzLWJhZTYtYjU2Y2Q3MjRlZjNiIiwicyI6Ik16RTBNamd5TWpBdE4yVTNaaTAwTmpRM0xUZ3hPRFl0TVRGa016TmtNVEU0WWpRMSJ9')
+ARGO_PORT = int(os.environ.get('ARGO_PORT', '20777'))
+CFIP = os.environ.get('CFIP', 'www.alive.com')
+CFPORT = int(os.environ.get('CFPORT', '443'))
+NAME = os.environ.get('NAME', 'Vls')
+CHAT_ID = os.environ.get('CHAT_ID', '')
+BOT_TOKEN = os.environ.get('BOT_TOKEN', '')
+PORT = int(os.environ.get('SERVER_PORT') or os.environ.get('PORT') or 3000)
 
 # Create running folder
 def create_directory():
